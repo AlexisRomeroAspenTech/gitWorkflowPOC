7a81fd7c98229e15e5b52e239c5404afdba3c2e6
Max Sanchez <max.sanchez@aspentech.com>
2021-05-24 10:54:54 -0500

    adding new feature

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NewFeatureComponent } from './new-feature/new-feature.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewFeatureComponent
   ],
   imports: [
     BrowserModule
